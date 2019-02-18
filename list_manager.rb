7f55fe9f2e00bd37f5ce44b3b5fccb2801cbcf3e
Nate Bierdeman <natebierdeman@gmail.com>
2019-02-17 20:27:45 -0600

    Move methods to ListManager module

---
+++ list_manager.rb
@@ -0,0 +1,21 @@
+module ListManager
+  def add_item(task, notes=nil, location=nil, complete=false)
+    list << ToDo.new(task, notes, location, complete)
+  end
+  def remove_item(identifier)
+    list.delete_if { |to_do| to_do.values.include?(identifier) }
+  end
+  def remove_all
+    list.clear
+  end
+  def mark_complete(identifier, status=true)
+    list.each do |to_do|
+      to_do.complete = status if to_do.values.include?(identifier)
+    end
+  end
+  def find(identifier)
+      list.collect do |to_do|
+        to_do if to_do.values.include?(identifier)
+      end.compact
+  end
+end
--- to_do_list.rb
+++ to_do_list.rb
@@ -1,27 +1,10 @@
 require_relative 'to_do'
+require_relative 'list_manager'
 class ToDoList
+  include ListManager
   attr_accessor :name, :list
   def initialize(name="To-do")
     @name = name
     @list = []
-  end
-  def add_item(task, notes=nil, location=nil, complete=false)
-    list << ToDo.new(task, notes, location, complete)
-  end
-  def remove_item(identifier)
-    list.delete_if { |to_do| to_do.values.include?(identifier) }
-  end
-  def remove_all
-    list.clear
-  end
-  def mark_complete(identifier, status=true)
-    list.each do |to_do|
-      to_do.complete = status if to_do.values.include?(identifier)
-    end
-  end
-  def find(identifier)
-      list.collect do |to_do|
-        to_do if to_do.values.include?(identifier)
-      end.compact
   end
 end
