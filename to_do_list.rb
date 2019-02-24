4271a42516403eb253ee8b21213cf558b2fac754
Nate Bierdeman <natebierdeman@gmail.com>
2019-02-24 16:12:37 -0600

    Add 'find' class method

--- to_do_list.rb
+++ to_do_list.rb
@@ -7,4 +7,37 @@ class ToDoList
     @name = name
     @list = []
   end
+  class << self
+    def find(identifier)
+      all_to_do_lists = []
+      found_to_dos = []
+      found_lists = []
+      # create array of all to-do lists
+      ObjectSpace.each_object(self) do |instance|
+        all_to_do_lists << instance
+      end
+      # create array of to-do's that match the identifier
+      all_to_do_lists.each do |to_do_list|
+        to_do_list.list.each do |to_do|
+          found_to_dos << to_do if to_do.values.include?(identifier)
+        end
+      end
+      # create array of to-do lists that have to-do's that match the identifier
+      found_to_dos.each do |to_do|
+        found_lists << all_to_do_lists.select do |to_do_list|
+          to_do_list if to_do_list.list.include?(to_do)
+        end
+      end
+      found_lists.flatten!
+      # output user message
+      if found_to_dos.any?
+        found_to_dos.each do |to_do|
+          puts "'#{to_do.task}' found in '#{found_lists[0].name}.'"
+          found_lists.shift
+        end
+      else
+        puts "No match found."
+      end
+    end
+  end
 end
