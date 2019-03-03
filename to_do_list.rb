685fc5c6a87e06bbef4582c3e95d6d4bea9f7e4c
Nate Bierdeman <natebierdeman@gmail.com>
2019-03-03 15:40:18 -0600

    Add found_list_names array

--- to_do_list.rb
+++ to_do_list.rb
@@ -10,16 +10,18 @@ class ToDoList
   class << self
     def find(identifier)
       all_to_do_lists = []
+      found_list_names = []
       found_lists = []
       found_to_dos = []
       # create 'all_to_do_lists' array of all to-do lists
       ObjectSpace.each_object(self) do |instance|
         all_to_do_lists << instance
       end
+      # create 'found_list_names' array of list names that match the identifier
       # create 'found_lists' array of to-do lists that match the identifier
       # create 'found_to_dos' array of to-do's that match the identifier
       all_to_do_lists.each do |to_do_list|
-        found_lists << to_do_list if to_do_list.name == identifier
+        found_list_names << to_do_list if to_do_list.name == identifier
         to_do_list.list.each do |to_do|
           found_lists << to_do_list if to_do.values.include?(identifier)
           found_to_dos << to_do if to_do.values.include?(identifier)
